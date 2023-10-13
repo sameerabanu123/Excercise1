5ce66710e570f52f468471ccfbfdfbd5c0b08e6a
sameerabanu123 <147610575+sameerabanu123@users.noreply.github.com>
2023-10-13 07:29:39 +0530

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,10 @@
+#Task-4th oct
+name=input("enter the user name:")
+print(name)
+
+firstname=input("enter the fname:")
+lastname=input("enter the lname:")
+print(firstname,lastname,sep="-",end="\t")
+
+name=input("enter the name:")
+print("You are welcome" ,name ,"to the Python Class.")
