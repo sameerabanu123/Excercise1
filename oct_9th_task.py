ea014fac2755af2e47fb8e93dd63a105f688cd41
sameerabanu123 <147610575+sameerabanu123@users.noreply.github.com>
2023-10-23 16:47:58 +0530

    Create oct_9th_task.py

---
+++ oct_9th_task.py
@@ -0,0 +1,20 @@
+#Remove Duplicates from list and convert in Set
+
+mylst=input("Enter n value : ").split()
+print(mylst)
+list1=set(mylst)
+print(list1)
+-----------or------------------
+n1=int(input("Enter the Number:"))
+n2=int(input("Enter the Number:"))
+n3=int(input("Enter the Number:"))
+n4=int(input("Enter the Number:"))
+n5=int(input("Enter the Number:"))
+set1=set()
+set1.add(n1)
+set1.add(n2)
+set1.add(n3)
+set1.add(n4)
+set1.add(n5)
+print(set1)
+
